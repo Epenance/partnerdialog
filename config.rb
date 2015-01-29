600a8568c2079b2c5fafbe9353d36a472e4350a1
Martin Hobert <admin@dosh.dk>
2015-01-29 15:06:37 +0100

    Starting wordpress convertion

--- config.rb
+++ config.rb
@@ -7,6 +7,7 @@ css_dir = "www/css"
 sass_dir = "scss"
 images_dir = "www/img"
 javascripts_dir = "www/js"
+output_style = :compressed
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -15,7 +16,7 @@ javascripts_dir = "www/js"
 relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
